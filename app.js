e0824ff42b7f39dd49bba10d1be65aae6f1ef235
justin-0 <itsjustinoconnell@gmail.com>
2025-04-18 10:15:54 +0100

    http methods and lazy router method

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import createApplication from "express.js";
+import methods from "methods";
 
 let app = createApplication();
+const slice = Array.prototype.slice();
 
 app.init = function () {
   this.cache = {};
@@ -9,3 +11,20 @@ app.init = function () {
 
   this._router = undefined;
 };
+
+app.lazyrouter = lazyrouter() => {
+  if(!this.router){
+    this.router = new Router({})
+  }
+}
+
+methods.forEach((method) => {
+  app[method] = (path) => {
+    this.lazyrouter();
+
+    const route = this._router.route(path);
+
+    route[method].apply(route, slice.call(arguments, 1));
+    return this;
+  };
+});
